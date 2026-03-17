62d09547d0beb9c4b9667ea3b3cf844cb6db9568
Vinicius Martins <costa.vinicius2010@gmail.com>
2026-03-17 18:23:18 -0400

    atualizao

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { verifyTokenEdge } from "./lib/auth-edge";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   // 🔓 LIBERA TUDO EM DEV
   if (process.env.NODE_ENV !== "production") {
     return NextResponse.next();
