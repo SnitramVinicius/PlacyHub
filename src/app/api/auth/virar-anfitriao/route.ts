bca9583df5c12b11b4362bd70ba42ae13a3d25b5
Vinicius Martins <costa.vinicius2010@gmail.com>
2026-03-17 16:40:15 -0400

    atualizao

--- src/app/api/auth/virar-anfitriao/route.ts
+++ src/app/api/auth/virar-anfitriao/route.ts
@@ -1,6 +1,7 @@
 //*-------- Transforma um LOCATÁRIO em LOCATÁRIO + ANFITRIÃO---------------*//
 import { NextResponse } from "next/server";
 import { verifyToken, signToken } from "@/lib/auth";
+import { cookies } from 'next/headers';
 
 export async function POST(req: Request) {
   const cookieStore = await cookies();
