46dd09eea1f1f70633a75e3e8ea9029abae8b292
Your Name <you@example.com>
2026-04-15 12:23:18 -0300

    atualizacao_de layout_p/correcao_de_erro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "sonner";
 import { FavoritosProvider } from "@/context/FavoritosContext";
 import { TemaProvider } from "@/context/TemaContext";
 import HelpButton from "@/components/HelpButton";
+import { Suspense } from "react";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -39,7 +40,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <TemaProvider>
           <AuthProvider>
             <FavoritosProvider>
-              <NavbarWrapper />
+              <Suspense fallback={null}>
+  <NavbarWrapper />
+</Suspense>
               {children}
               <Toaster />
               <HelpButton />
